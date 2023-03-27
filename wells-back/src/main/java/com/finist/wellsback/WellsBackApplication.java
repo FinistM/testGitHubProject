eaeb4ba3449d46a8c994bcaf1934a5da7332364b
FinistM <129047890+FinistM@users.noreply.github.com>
2023-03-27 14:44:14 +0500

    Add files via upload

---
+++ wells-back/src/main/java/com/finist/wellsback/WellsBackApplication.java
@@ -0,0 +1,11 @@
+package com.finist.wellsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellsBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WellsBackApplication.class, args);
+	}
+}
---
+++ wells-back/src/test/java/com/finist/wellsback/WellsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finist.wellsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
